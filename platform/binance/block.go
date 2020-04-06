297b0b78a68256e8c704a0708d3a7ddee1aab914
Mykola <kolya182@gmail.com>
2020-04-05 19:11:03 -0700

    Clean up

--- platform/binance/block.go
+++ platform/binance/block.go
@@ -14,18 +14,38 @@ func (p *Platform) CurrentBlockNumber() (int64, error) {
 }
 
 func (p *Platform) GetBlockByNumber(num int64) (*blockatlas.Block, error) {
-	//blockTxs, err := p.client.GetBlockTransactions(num)
-	//if err != nil {
-	//	return nil, err
-	//}
+	blockTxs, err := p.client.GetBlockTransactions(num)
+	if err != nil {
+		return nil, err
+	}
 
 	txs := make(blockatlas.TxPage, 0)
-	//childTxs, err := p.getTxChildChan(blockTxs)
-	//if err == nil {
-	//	txs = NormalizeTxs(childTxs, "", "")
-	//} else {
-	//	txs = NormalizeTxs(blockTxs, "", "")
-	//}
+	childTxs := make([]Tx, 0)
+	for _, t := range blockTxs {
+		if len(t.SubTransactions) > 0 {
+			for _, tSub := range t.SubTransactions {
+				newT := Tx{
+					Asset:       tSub.Asset,
+					BlockHeight: t.BlockHeight,
+					Code:        t.Code,
+					Data:        t.Data,
+					Fee:         tSub.Fee,
+					FromAddr:    tSub.FromAddr,
+					Memo:        t.Memo,
+					OrderID:     t.OrderID,
+					Sequence:    t.Sequence,
+					Source:      t.Source,
+					Timestamp:   t.Timestamp,
+					ToAddr:      tSub.ToAddr,
+					TxHash:      t.TxHash,
+					Type:        tSub.Type,
+					Value:       tSub.Value,
+				}
+				childTxs = append(childTxs, newT)
+			}
+		}
+	}
+	txs = NormalizeTxs(childTxs, "", "")
 	return &blockatlas.Block{
 		Number: num,
 		Txs:    txs,
